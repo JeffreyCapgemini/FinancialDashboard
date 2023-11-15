88369b97a7f4c70d9ef921be4eec4482afaec814
Desley Aalderink <desleyaalderink@Desleys-MacBook-Pro.local>
2023-11-15 12:13:04 +0100

    feat: 🎸 income feature with graphs
    Income feature with graphs

--- financial-dashboard-app/src/app/app.module.ts
+++ financial-dashboard-app/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { IncomeSourceComponent } from './income-source/income-source.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IncomeSourceComponent
   ],
   imports: [
     BrowserModule,
